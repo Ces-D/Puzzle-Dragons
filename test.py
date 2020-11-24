a9e3d59114852039069bae966e56939259b2cc5f
Cesar <cesdia883@gmail.com>
2020-11-24 17:09:26 -0500

    Monster Box method creating box done

--- test.py
+++ test.py
@@ -9,7 +9,15 @@
 
 
 from monsters import MonsterStats
+from pad import PuzzlesDragons, UpdatedMonstersContent
+from Player.monster_box import MonsterBox
 
-monster_profile = MonsterStats.monster_profile(monster_id="4722")
+# monster_profile = MonsterStats.monster_profile(monster_id="6596")
 
-print(monster_profile)
+# print(monster_profile)
+
+# # updated_c = UpdatedMonstersContent(PuzzlesDragons.read_home_page_soup())
+# # print(updated_c.updated_monster_links())
+
+
+MonsterBox.create_monster_box()
